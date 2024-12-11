48e74b7048a5e82f310c16236eb5b7bce6e5b6bd
muhammadaakifraza <simply.aakif@gmail.com>
2024-12-11 18:13:52 +0500

    Ep 1 - End

---
+++ resources/views/frontend/products/index.blade.php
@@ -0,0 +1,8 @@
+<ul>
+
+@foreach($products as $product)
+    <li>
+    {{$product->name}}
+    </li>
+@endforeach
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])
+    ->name('products.index');
+
+Route::get('/blogs', [BlogController::class, 'index'])
+    ->name('blogs.index');
 
 Route::middleware([
     'auth:sanctum',
